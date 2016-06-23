a060b2825928b0ab55fe4ad9538f6b0141eb8c8e
Rowan Parkinson <rowan.parkinson@gmail.com>
2016-06-24 07:14:40 +1000

    Removing WP base files

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header(); ?>
+	<div id="content">
+		<?php if ( ! dynamic_sidebar( 'Alert' ) ) : ?>
+			<!--Wigitized 'Alert' for the home page -->
+		<?php endif ?>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="post-single">
+				<article class="content-width">
+					<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<div class="post-content">
+						<?php the_content(__('Read more'));?>
+					</div>
+				</article>
+			</div><!--.post-single-->
+		<?php endwhile; else: ?>
+			<div class="no-results">
+				<p><strong><?php _e('There has been an error.'); ?></strong></p>
+				<p><?php _e('We apologize for any inconvenience, please hit back on your browser or use the search form below.'); ?></p>
+				<?php get_search_form(); /* outputs the default Wordpress search form */ ?>
+			</div><!--noResults-->
+		<?php endif; ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+		<div class="oldernewer">
+			<p class="older"><?php next_posts_link('&laquo; Older Entries') ?></p>
+			<p class="newer"><?php previous_posts_link('Newer Entries &raquo;') ?></p>
+		</div><!--.oldernewer-->
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	</div><!--#content-->
+<?php get_footer(); ?>
